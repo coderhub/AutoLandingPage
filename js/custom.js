06f010e742e33366d800e4673e34feba89285e2e
cmerced <mail.merced@gmail.com>
2019-02-17 17:12:10 -0500

    Landing files

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// $(function () {
+//   var viewer = ImageViewer();
+//   $('.zoomimg').click(function () {
+//       var imgSrc = this.src,
+//           highResolutionImage = $(this).data('high-res-img');
+
+//       viewer.show(imgSrc, highResolutionImage);
+//   });
+// });
